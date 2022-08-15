b97421f1ac12062342b9106d99cac795fa50d7d4
ashbar409 <ashbar409@gmail.com>
2022-08-15 16:43:19 -0400

    Created component for Florida Man article api call

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import "./App.css";
 import Footer from "./components/shared/Footer";
 import Header from "./components/shared/Header";
 import Main from "./components/shared/Main";
+import Article from "./components/Article"
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Main />
+      {/* <Main /> */}
+      {/* Can be made to take a year as well, format for date is currently MM-DD*/}
+      <Article date="08-15"/> 
+      
       <Footer />
+      
     </div>
   );
 }
--- src/components/shared/Main.js
+++ src/components/shared/Main.js
@@ -16,6 +16,7 @@ export default function Main() {
   return (
     <main>
       <UserForm />
+      
     </main>
   );
 }
