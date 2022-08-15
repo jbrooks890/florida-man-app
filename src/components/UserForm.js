e1672d16204b4c8845183e91e80cb1b7a440366d
ArcanBoi <61268478+ArcanBoi@users.noreply.github.com>
2022-08-14 22:22:04 -0400

    filled out names and animals in utility

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -6,7 +6,7 @@ export default function UserForm({ onSubmit }) {
   const [selectedMonth, setSelectedMonth] = useState(_m[0]);
   const currYear = new Date().getFullYear();
 
-  console.log(currYear);
+  //   console.log(currYear);
 
   const popYears = () => {
     const arr = [];
@@ -21,31 +21,52 @@ export default function UserForm({ onSubmit }) {
   };
 
   return (
-    <form id="user-form">
+    <form
+      id="user-form"
+      onSubmit={(e) => {
+        e.preventDefault();
+        console.log(e);
+      }}
+    >
       <h3>Florida Man</h3>
       <div className="form-name">
-        <label htmlFor="firstName">
+        <label htmlFor="first-name">
           <p>First name</p>
-          <input id="firstName" />
+          <input id="first-name" name="first-name" />
         </label>
-        <label htmlFor="lastName">
+        <label htmlFor="last-name">
           <p>Last name</p>
-          <input id="lastName" />
+          <input id="last-name" name="last-name" />
         </label>
       </div>
       <div id="user-dob">
         <label>
           <p>Birthday</p>
           <div>
-            <select onChange={(e) => setSelectedMonth(e.currentTarget.value)}>
+            <select
+              id="birth-month"
+              name="birth-month"
+              onChange={(e) => setSelectedMonth(e.currentTarget.value)}
+            >
               {_m.map((month, i) => (
                 <option key={i} value={month}>
                   {month}
                 </option>
               ))}
             </select>
-            <input type="number" min="1" max={months.get(selectedMonth)} />
-            <select name="year" defaultValue={currYear}>
+            <input
+              id="birth-day"
+              name="birth-day"
+              type="number"
+              min="1"
+              max={months.get(selectedMonth)}
+              defaultValue="1"
+            />
+            <select
+              id="birth-year"
+              name="birth-year"
+              defaultValue={currYear - 18}
+            >
               {popYears()}
             </select>
           </div>
