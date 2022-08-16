0a774e783639e731d98cf301e4a6c25be34acbb8
Julian Brooks <julian@Julians-Air.attlocal.net>
2022-08-16 02:00:39 -0400

    styling

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,16 @@ export default function About() {
   return (
     <div id="about-page">
       <h1>About</h1>
+      <p>
+        Have you ever wanted an all in one Florida Man generator and astrology
+        website? Now you can! Get your horoscopes and your own personal Florida
+        Man without having to leave our site, all you need to do is input your
+        name and date of birth to see what your Florida Man is up to and what
+        your daily horoscope looks like! He might be threatening to take over
+        the town with an army of turtles after leaving the police station, he
+        might be wrestling alligators while parasailing. Who knows what they
+        could be up to, so find out today!
+      </p>
     </div>
   );
 }
